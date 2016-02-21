016c2ac6de27dbd3b7f4b362c9927d6e101825b6
Mathieu Boisnard <m.boisnard@free.fr>
2016-02-21 15:49:34 +0100

    RC2S-23 Contexte Hibernate
    - Contexte foireux

--- rc2s-dao/src/main/java/com/rc2s/dao/UserDAO.java
+++ rc2s-dao/src/main/java/com/rc2s/dao/UserDAO.java
@@ -2,15 +2,32 @@ package com.rc2s.dao;
 
 import com.rc2s.common.vo.User;
 import java.util.ArrayList;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDAO implements IUserDAO
 {  
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    
     @Override
     public ArrayList<User> getUsers()
     {
-        //return (ArrayList<User>) sessionFactory.getCurrentSession().createQuery("from User").list();
+        if(sessionFactory == null)
+        {
+            throw new IllegalStateException("coucou");
+        }
+        Session session = sessionFactory.openSession();
+        session.close();
+        //return (ArrayList<User>) sessionFactory.getCurrentSession().createQuery("select u from User as u").list();
         ArrayList<User> list = new ArrayList<>();
         list.add(new User("Mathieu", "azeaze"));
         list.add(new User("Vincent", "ztzt"));
