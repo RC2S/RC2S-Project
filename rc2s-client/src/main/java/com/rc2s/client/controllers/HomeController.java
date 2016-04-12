4b769f57888cbea9dddfd7a7631f1eccaa647526
Mathieu Boisnard <m.boisnard@free.fr>
2016-04-12 22:15:28 +0200

    Revert "RC2S-48 Login"
    This reverts commit 0da99665cfa2d6b64dac7ce707b6b993a340db2b.

--- rc2s-client/src/main/java/com/rc2s/client/controllers/HomeController.java
+++ rc2s-client/src/main/java/com/rc2s/client/controllers/HomeController.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.rc2s.client.controllers;
 
 import com.rc2s.client.utils.Resources;
