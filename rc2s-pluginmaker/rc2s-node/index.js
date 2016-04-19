adc1cfbba5a94a52c535ec920662aeb7f34fbb24
Vincent Milano <vincent_mot@hotmail.fr>
2016-04-19 21:31:22 +0200

    [RC2S-69] Structure Application NodeJS + Minor fixes on index.js

--- rc2s-pluginmaker/rc2s-node/index.js
+++ rc2s-pluginmaker/rc2s-node/index.js
@@ -1,1 +1,17 @@
-require("./express")();
+//require("./express")();
+
+var http = require("http");
+
+var options = {
+  host: '192.168.209.128',
+  port: 8080,
+  path: '/api/workspace?skipCount=0&maxItems=30'
+};
+
+http.get(options, function(res) {
+  console.log('STATUS : ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  console.log('BODY   : ' + res.body);
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
