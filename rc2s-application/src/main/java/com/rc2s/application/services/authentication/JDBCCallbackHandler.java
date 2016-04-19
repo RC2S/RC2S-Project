27da2541771011a6471502287fcf030f61ba0884
Mathieu Boisnard <m.boisnard@free.fr>
2016-04-19 21:29:16 +0200

    RC2S-26 Service d'authentification
    - Modification Jaas, CallbackHandler, ...

--- rc2s-application/src/main/java/com/rc2s/application/services/authentication/JDBCCallbackHandler.java
+++ rc2s-application/src/main/java/com/rc2s/application/services/authentication/JDBCCallbackHandler.java
@@ -3,12 +3,14 @@ package com.rc2s.application.services.authentication;
 import java.io.IOException;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
 public class JDBCCallbackHandler implements CallbackHandler
 {
-    private String username;
-    private String password;
+    private final String username;
+    private final String password;
     
     public JDBCCallbackHandler(String username, String password)
     {
@@ -19,6 +21,22 @@ public class JDBCCallbackHandler implements CallbackHandler
     @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException
     {
-        
+        for(int i = 0; i < callbacks.length; i++)
+        {
+			if(callbacks[i] instanceof NameCallback)
+            {
+				NameCallback nameCallback = (NameCallback) callbacks[i];
+				nameCallback.setName(username);
+			}
+            else if(callbacks[i] instanceof PasswordCallback)
+            {
+				PasswordCallback passwordCallback = (PasswordCallback) callbacks[i];
+				passwordCallback.setPassword(password.toCharArray());
+			}
+            else
+            {
+				throw new UnsupportedCallbackException(callbacks[i], "The submitted Callback is unsupported");
+			}
+		}
     }
 }
