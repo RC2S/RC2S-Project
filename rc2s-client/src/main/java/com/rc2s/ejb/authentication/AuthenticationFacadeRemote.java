489f1a8e0d0f269bf10988f28392c94cdb0d6c51
Mathieu Boisnard <m.boisnard@free.fr>
2016-04-23 11:31:02 +0200

    RC2S-58 Processeur d'annotations
    - Clean branche

--- rc2s-client/src/main/java/com/rc2s/ejb/authentication/AuthenticationFacadeRemote.java
+++
@@ -1,9 +0,0 @@
-package com.rc2s.ejb.authentication;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface AuthenticationFacadeRemote
-{
-    public boolean login();
-}
--- rc2s-client/src/main/java/com/rc2s/ejb/user/UserFacadeRemote.java
+++
@@ -1,9 +0,0 @@
-package com.rc2s.ejb.user;
-
-import com.rc2s.common.vo.User;
-import java.util.List;
-
-public interface UserFacadeRemote
-{
-    public List<User> getAllUsers();
-}
