c30ccc7de6309067e9f20a72ba5d75de920e08b8
Vincent Milano <vincent_mot@hotmail.fr>
2016-05-03 21:32:07 +0200

    RC2S-86 Annotation @SourceControl - Package name is verified - ClassNamesEnum, pluginName, entityName (for app, dao & ejb) are retrieved - Commented code -> two questions 	-> regex on pluginName & entityName ? 	-> lowerCase verification ?

--- rc2s-annotations/src/main/java/com/rc2s/annotations/processors/SourceControlProcessor.java
+++ rc2s-annotations/src/main/java/com/rc2s/annotations/processors/SourceControlProcessor.java
@@ -53,9 +53,11 @@ public class SourceControlProcessor extends AbstractProcessor
             {
 				if(annotated.getKind() == ElementKind.CLASS)
 				{
+					mainClass = analysor.classAnalysor(annotated);
+					
 					try
 					{
-						SourceUtil.verifySource(analysor.classAnalysor(annotated));
+						SourceUtil.verifySource(mainClass);
 					}
 					catch (Exception ex)
 					{
@@ -88,26 +90,26 @@ public class SourceControlProcessor extends AbstractProcessor
 	private void tests(Element annotated)
 	{
 		// Get the name of annotated element
-		messager.printMessage(Diagnostic.Kind.NOTE, annotated.getSimpleName());
+		messager.printMessage(Diagnostic.Kind.NOTE, annotated.getSimpleName()); // Main
 
 		// Get the type of annotated element
-		messager.printMessage(Diagnostic.Kind.NOTE, "type : " + annotated.getKind().toString());
+		messager.printMessage(Diagnostic.Kind.NOTE, "type : " + annotated.getKind().toString()); // CLASS
 
 		// Get the full name of annotated element
-		messager.printMessage(Diagnostic.Kind.NOTE, "full name with package : " + annotated.asType().toString());
+		messager.printMessage(Diagnostic.Kind.NOTE, "full name with package : " + annotated.asType().toString()); // com.rc2s.client.Main
 
 		// Get all elements into the annotated element (ex : methods, fields, constructors in the annotated class)
 		List<? extends Element> el = annotated.getEnclosedElements();
 		for(Element e : el) {
-			messager.printMessage(Diagnostic.Kind.NOTE, "test : " + e.getSimpleName().toString());
+			messager.printMessage(Diagnostic.Kind.NOTE, "test : " + e.getSimpleName().toString()); // <init>, stage, main, start, stop
 		}
 
 		// Get the parent element of the annotated element 
 		// (package name if annotated element is a class, class name if annotated element is field, constructor, method)
-		messager.printMessage(Diagnostic.Kind.NOTE, "enclosing : " + annotated.getEnclosingElement().getSimpleName().toString());
+		messager.printMessage(Diagnostic.Kind.NOTE, "enclosing : " + annotated.getEnclosingElement().getSimpleName().toString()); // client
 
 		// Get the Package of annotated element
-		messager.printMessage(Diagnostic.Kind.NOTE, elementUtils.getPackageOf(annotated).toString());
+		messager.printMessage(Diagnostic.Kind.NOTE, elementUtils.getPackageOf(annotated).toString()); // com.rc2s.client
 
 		// Get the annotation properties for annotated element
 		for (AnnotationMirror annotationMirror : annotated.getAnnotationMirrors())
--- rc2s-annotations/src/main/java/com/rc2s/annotations/utils/SourceUtil.java
+++ rc2s-annotations/src/main/java/com/rc2s/annotations/utils/SourceUtil.java
@@ -6,36 +6,134 @@ public class SourceUtil
 {	
 	public static void verifySource(ElementMapper mainClass)
 	{
-		// Verify package construction
-		String[] packageParts = mainClass.getPackageName().split(".");
+		String[] packageParts = mainClass.getPackageName().split("\\.");
 		
-		System.err.println("Package parts :");
-		for (String part : packageParts)
-		{
-			System.err.println("  -> " + part);
-		}
-		
-		/*
 		try
 		{
-			verifyPackageNaming(packageParts);
+			// Verify root - shall be com.rc2s.{plugin_name}
+			String pluginName = verifyRoot(packageParts);
+		
+			// Then find class type
+			ClassNamesEnum cne = findClassName(packageParts);
+		
+			// Then verify remaining package parts depending on ClassNamesEnum retrieved
+			String entityName = null;
+			if (ClassNamesEnum.APPLICATION.equals(cne)
+				|| ClassNamesEnum.DAO.equals(cne)
+				|| ClassNamesEnum.EJB.equals(cne))
+			{
+				entityName = verifyPackageEnd(packageParts);
+			}
+			
+			// Then verify necessary class things (name, annotations..)
+			verifyClassStandards(mainClass, cne, entityName);
 		}
 		catch (Exception ex)
 		{
 			ex.printStackTrace();
 		}
-		*/
+	}
+
+	private static String verifyRoot(String[] packageParts)
+	{
+		String name = null;
 		
-		// Verify class name
-		/*
-		try
+		if (packageParts.length < 3)
+			throw new UnsupportedOperationException("Not supported yet.");
+		
+		// Check lowercase and without lowercase ? "Shall be lowercase"
+		if (packageParts[0].toLowerCase().equals("com") && packageParts[1].toLowerCase().equals("rc2s"))
 		{
-			verifyClassName(mainClass.name);
+			// Shall check with regex ([a-zA-Z][-_a-zA-Z]+) ?
+			name = packageParts[2];
 		}
-		catch (Exception ex)
+		else
+			throw new UnsupportedOperationException("Not supported yet.");
+		
+		return name;
+	}
+
+	private static ClassNamesEnum findClassName(String[] packageParts)
+	{
+		if (packageParts.length < 4)
+			throw new UnsupportedOperationException("Not supported yet.");
+		else if (packageParts.length < 5)
+			throw new UnsupportedOperationException("Not supported yet.");
+		
+		// Idem check lowercase ?
+		switch (packageParts[3].toLowerCase()) 
 		{
-			ex.printStackTrace();
+			case "ejb":
+				return ClassNamesEnum.EJB;
+				
+			case "application":
+				return ClassNamesEnum.APPLICATION;
+				
+			case "dao":
+				return ClassNamesEnum.DAO;
+				
+			case "common":
+				return CommonClassNameFromPackage(packageParts[4]);
+				
+			case "client":
+				return ClientClassNameFromPackage(packageParts[4]);
+				
+			default:
+				throw new UnsupportedOperationException("Not supported yet.");
 		}
-		*/
+	}
+	
+	private static ClassNamesEnum CommonClassNameFromPackage(String packagePart)
+	{
+		// Idem check lowercase ?
+		switch (packagePart.toLowerCase()) 
+		{
+			case "vo":
+				return ClassNamesEnum.VO;
+				
+			case "sql":
+				return ClassNamesEnum.SQL;
+				
+			default:
+				throw new UnsupportedOperationException("Not supported yet.");
+		}
+	}
+
+	private static ClassNamesEnum ClientClassNameFromPackage(String packagePart)
+	{
+		// Idem check lowercase ?
+		switch (packagePart.toLowerCase()) 
+		{
+			case "controllers":
+				return ClassNamesEnum.CONTROLLERS;
+				
+			case "views":
+				return ClassNamesEnum.VIEWS;
+				
+			case "css":
+				return ClassNamesEnum.CSS;
+				
+			case "images":
+				return ClassNamesEnum.IMAGES;
+				
+			case "utils":
+				return ClassNamesEnum.UTILS;
+				
+			default:
+				throw new UnsupportedOperationException("Not supported yet.");
+		}
+	}
+	
+	private static String verifyPackageEnd(String[] packageParts)
+	{
+		if (packageParts.length < 5)
+			throw new UnsupportedOperationException("Not supported yet.");
+		
+		return packageParts[4];
+	}
+
+	private static void verifyClassStandards(ElementMapper mainClass, ClassNamesEnum cne, String entityName)
+	{
+		
 	}
 }
