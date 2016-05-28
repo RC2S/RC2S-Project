b81aa907b991aceb6a3f2aa5941eece04b29750c
Mathieu Boisnard <m.boisnard@free.fr>
2016-05-28 19:59:59 +0200

    RC2S-97 Création du projet War
    - Création du jnlp
    - Signature des librairies
    - Création du War
    - Déploiement automatique du war

--- rc2s-pluginmaker/express/utils/LogFilesUtils.js
+++ rc2s-pluginmaker/express/utils/LogFilesUtils.js
@@ -1,11 +1,11 @@
 var log4js = require('log4js');
 var config = require('./config');
 
-log4js.loadAppender('file');
 var httpLogsPath 	= config.log4js.dirname + config.log4js.httpLogName;
 var authLogsPath 	= config.log4js.dirname + config.log4js.authLogName;
 var dbLogsPath 		= config.log4js.dirname + config.log4js.dbLogName;
 
+log4js.loadAppender('file');
 log4js.addAppender(log4js.appenders.file(httpLogsPath), config.log4js.httpLogName);
 log4js.addAppender(log4js.appenders.file(authLogsPath), config.log4js.authLogName);
 log4js.addAppender(log4js.appenders.file(dbLogsPath), config.log4js.dbLogName);
