cf03612213ee17fc96764b61a2b0970e3014e8c9
Valentin Fries <contact@fries.io>
2016-06-02 15:14:59 +0200

    RC2S-83 Listener UDP
     - Création du listener UDP sur le daemon
     - Création du PacketProcessor qui va traiter les packets en provenance du
       serveur d'application
     - Réponse au probe de status de la part du serveur d'appli

---
+++ rc2s-daemon/src/main/java/com/rc2s/daemon/network/PacketProcessor.java
@@ -0,0 +1,68 @@
+package com.rc2s.daemon.network;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class PacketProcessor extends Thread
+{
+	private final DatagramSocket socket;
+	private final DatagramPacket packet;
+	
+	/**
+	 * Init a new PacketProcessor in its own thread in order to handle the
+	 * packet's data.
+	 * @param socket
+	 * @param packet 
+	 */
+	public PacketProcessor(DatagramSocket socket, DatagramPacket packet)
+	{
+		this.socket = socket;
+		this.packet = packet;
+	}
+	
+	/**
+	 * Run the thread and process the correct action.
+	 */
+	@Override
+	public synchronized void run()
+	{
+		String raw = new String(packet.getData());
+		System.out.println("RECEIVED: " + raw);
+		
+		switch(raw)
+		{
+			case "status": validateStatus(); break;
+			default: processPacket(); break;
+		}
+	}
+	
+	/**
+	 * Process the packet in order to obtain a valid signal.
+	 */
+	private void processPacket()
+	{
+		// TODO
+	}
+	
+	/**
+	 * Notify the application server that the daemon is up and running.
+	 */
+	private void validateStatus()
+	{
+		try
+		{
+			InetAddress clientIp = packet.getAddress();
+			int clientPort = packet.getPort();
+			byte[] buffer = "up".getBytes();
+
+			DatagramPacket statusPacket = new DatagramPacket(buffer, buffer.length, clientIp, clientPort);
+			socket.send(statusPacket);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
