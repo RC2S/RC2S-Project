dc0772bde00b9a6aa23c0b122b34b5b3481d04ff
Vincent Milano <vincent_mot@hotmail.fr>
2016-06-22 23:00:02 +0200

    RC2S-42 Création de l'algorithme Added tests for algo v0.2 Added AudioInputStream manipulations & audio file analysing

--- rc2s-application/src/main/java/com/rc2s/application/services/streaming/StreamingService.java
+++ rc2s-application/src/main/java/com/rc2s/application/services/streaming/StreamingService.java
@@ -1,11 +1,20 @@
 package com.rc2s.application.services.streaming;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 
 @Stateless
 public class StreamingService implements IStreamingService
@@ -13,8 +22,76 @@ public class StreamingService implements IStreamingService
 	@Override
 	public void streamMusic()
 	{
-		System.out.println("IN");
 		String music = "/media/Data/Datas/Music/Caravan_Palace/LoneDigger.mp3";
+		
+		//firstTest(music);
+		
+		trueTest(music);
+	}
+	
+	private void trueTest(String music)
+	{
+		System.out.println("------------------------------");
+		System.out.println("*** Running true test ***");
+		
+		File				soundFile;
+		InputStream			ins;
+		BufferedInputStream	buff;
+		AudioInputStream	ais;
+		SourceDataLine		line;
+		AudioFormat			fmt;
+		 
+		try
+		{
+			// Preparation
+            soundFile = new File(music);
+			System.out.println("111");
+			ins = new FileInputStream(soundFile);
+			System.out.println("222");
+			buff = new BufferedInputStream(ins);
+			System.out.println("333");
+			ais	= AudioSystem.getAudioInputStream(buff);
+			System.out.println("444");
+			byte[] bytes = new byte[(int) soundFile.length()];
+			ais.read(bytes);
+			
+			fmt	= ais.getFormat();
+			
+			DataLine.Info info	= new DataLine.Info(SourceDataLine.class, fmt);
+			line				= (SourceDataLine) AudioSystem.getLine(info);
+            line.open(fmt);
+			line.start();
+			
+			float[] samples = new float[100000];
+			//int retrievedSamples = 0;
+			
+			// Real thing
+			for (int blen = 0; (blen = ais.read(bytes)) > -1;) {
+				int slen;
+				slen = SimpleAudioConversion.unpack(bytes, samples, blen, fmt);
+
+				// Do something with samples
+
+				blen = SimpleAudioConversion.pack(samples, bytes, slen, fmt);
+				line.write(bytes, 0, blen);
+			}
+        }
+		catch (LineUnavailableException e)
+		{
+            e.printStackTrace();
+        }
+		catch (Exception e)
+		{
+            e.printStackTrace();
+        }
+		
+		
+	}
+	
+	private void firstTest(String music)
+	{
+		System.out.println("------------------------------");
+		System.out.println("*** Running first test ***");
 	
 		File f = new File(music);
 		int size = (int)f.length();
@@ -34,7 +111,7 @@ public class StreamingService implements IStreamingService
 			int count = 0;
 			
 			// Coordinates arraylist
-			ArrayList<Coordinates3D> coordinatesBufferlist = new ArrayList<>();
+			List<Coordinates3D> coordinatesBufferlist = new ArrayList<>();
 			
 			// Buffer length wanted for coordinates retrieving
 			final short BUFFER_LENGTH = 300;
