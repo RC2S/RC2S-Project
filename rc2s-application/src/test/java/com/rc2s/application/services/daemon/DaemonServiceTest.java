31a651a059b661b633c4a3aa70c032cb27b24077
Valentin Fries <contact@fries.io>
2016-07-23 16:54:32 +0200

    RC2S-121 Inversion tableaux de booléens
     - Inversion correcte sur les axes Y et X
     - Mise à jour du test unitaire correspondant

--- rc2s-application/src/test/java/com/rc2s/application/services/daemon/DaemonServiceTest.java
+++ rc2s-application/src/test/java/com/rc2s/application/services/daemon/DaemonServiceTest.java
@@ -44,23 +44,23 @@ public class DaemonServiceTest
 				{false, false, false, false},
 				{false, false, false, false},
 				{false, false, false, false},
-				{true, false, false, false}
+				{false, false, false, true}
 			},
 			{
 				{false, false, false, false},
 				{false, false, false, false},
-				{true, false, false, false},
+				{false, false, false, true},
 				{false, false, false, false}
 			},
 
 			{
 				{false, false, false, false},
-				{true, false, false, false},
+				{false, false, false, true},
 				{false, false, false, false},
 				{false, false, false, false}
 			},
 			{
-				{true, false, false, false},
+				{false, false, false, true},
 				{false, false, false, false},
 				{false, false, false, false},
 				{false, false, false, false}
